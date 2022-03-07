0a490a7adec0f2ec537b645ea45013eaffc2adba
NetaJM <netamilenkovic@gmail.com>
2022-03-07 14:30:02 +0200

    git commit

--- main.js
+++ main.js
@@ -49,10 +49,9 @@ document.querySelector('.search button').addEventListener('click', function () {
 });
 
 document.querySelector('.search-bar').addEventListener('keyup', function (event) {
-    if (event.key === 'enter') {
-        weather.search
+    if (event.key === 'Enter') {
+        weather.search();
     }
-});
+})
 
-
-weather.fetchWeather('Belgrade')
+weather.fetchWeather('Tel Aviv')
